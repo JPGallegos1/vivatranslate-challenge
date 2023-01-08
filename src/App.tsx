d9eb4b3c02a9b6972a544fadf47a0db0657f60d2
Juan Pablo Gallegos <jpgallegos20@gmail.com>
2023-01-08 13:30:05 -0300

    (#3) fix header hero image

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,10 @@
-import {useState} from "react";
-
-import reactLogo from "./assets/react.svg";
-import "./App.scss";
+import Header from "./components/Header/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <header className="header header__background">
-        <div className="header__content container">
-          <div className="header__bar">
-            <div className="header__logo">
-              <p>Viva Translate</p>
-            </div>
-
-            <nav className="header__navigation">
-              <a href="#">About</a>
-              <a href="#">Join</a>
-              <a href="#">Contact</a>
-            </nav>
-          </div>
-        </div>
-
-        <div className="header__heading">
-          <h1>we are breaking langueage's barriers</h1>
-        </div>
-      </header>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
